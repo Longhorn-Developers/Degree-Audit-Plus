1a53ee0849de7e465ffd53c6d228b8047c44dfea
samsinghh <samyak1113@gmail.com>
2025-10-25 22:46:42 -0500

    fix: make Open button on IDA popup redirect to Degree Audit Plus page

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,13 @@
+import { browser } from "wxt/browser";
+
 export default defineBackground(() => {
-	console.log("Hello background!", { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+
+  // Listen for messages from content scripts
+  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "open-dap") {
+      const url = browser.runtime.getURL("degree-audit.html" as any);
+      browser.tabs.create({ url });
+    }
+  });
 });
