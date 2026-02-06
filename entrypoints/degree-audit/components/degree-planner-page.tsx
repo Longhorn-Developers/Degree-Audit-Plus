5a0be5e0d9743252eb76a5a88ade920022097c93
Sidd03192 <siddharthpotta19@gmail.com>
2026-02-06 17:55:25 -0600

    Minor style updates + removed category-progress.tsx and used overflow update for semester card

--- entrypoints/degree-audit/components/degree-planner-page.tsx
+++ entrypoints/degree-audit/components/degree-planner-page.tsx
@@ -1,12 +1,11 @@
 import Button from "@/entrypoints/components/common/button";
 import {
-	HStack,
-	Substack,
-	VStack,
-	Wrap,
+  HStack,
+  VStack,
+  Wrap,
 } from "@/entrypoints/components/common/helperdivs";
 import Title from "@/entrypoints/components/common/text";
-import CourseCard from "@/entrypoints/components/course-card";
+import { CourseSearchContent } from "@/entrypoints/components/course-add-modal";
 import "@/entrypoints/styles/content.css";
 import { Course } from "@/lib/general-types";
 import { Plus } from "lucide-react";
@@ -14,91 +13,69 @@ import { useAuditContext } from "./audit-provider";
 import { SimpleDegreeCompletionDonut } from "./degree-completion-donut";
 import SemesterCard from "./semester-card";
 
-const CourseSuggestions = () => {
-	return (
-		<div className="w-sm p-6 rounded-lg border border-gray-200 bg-[#FAFAF9]">
-			<VStack fillHeight fillWidth y="stretch">
-				<Title text="Course Suggestions" className="m-0" />
-				<Substack>
-					<CourseCard
-						className="w-full"
-						fullName="Course Name"
-						courseName="Course Code"
-						color="orange"
-					/>
-					<CourseCard
-						className="w-full"
-						fullName="Course Name"
-						courseName="Course Code"
-						color="orange"
-					/>
-				</Substack>
-			</VStack>
-		</div>
-	);
-};
-
 const SidePanel = () => {
-	return (
-		<VStack fill className="h-full" y="stretch" x="center">
-			<SimpleDegreeCompletionDonut size={300} />
-			<CourseSuggestions />
-		</VStack>
-	);
+  return (
+    <VStack fill className="h-full" y="stretch" x="center">
+      <SimpleDegreeCompletionDonut size={300} />
+      <div className="w-sm mt-10 p-3 rounded-lg border border-gray-200 bg-[#FAFAF9]">
+        <CourseSearchContent />
+      </div>
+    </VStack>
+  );
 };
 
 const MainContent = () => {
-	const { allCourses } = useAuditContext();
+  const { allCourses } = useAuditContext();
 
-	const semesters = useMemo(() => {
-		return Object.entries(
-			allCourses.reduce(
-				(acc, course) => {
-					acc[course.semester] = [...(acc[course.semester] || []), course];
-					return acc;
-				},
-				{} as Record<string, Course[]>
-			)
-		).sort((a, b) => {
-			const [yearA, seasonA] = a[0].split(" ");
-			const [yearB, seasonB] = b[0].split(" ");
-			return Number(yearA) - Number(yearB) || seasonA.localeCompare(seasonB);
-		});
-	}, [allCourses]);
+  const semesters = useMemo(() => {
+    return Object.entries(
+      allCourses.reduce(
+        (acc, course) => {
+          acc[course.semester] = [...(acc[course.semester] || []), course];
+          return acc;
+        },
+        {} as Record<string, Course[]>,
+      ),
+    ).sort((a, b) => {
+      const [yearA, seasonA] = a[0].split(" ");
+      const [yearB, seasonB] = b[0].split(" ");
+      return Number(yearA) - Number(yearB) || seasonA.localeCompare(seasonB);
+    });
+  }, [allCourses]);
 
-	const AddSemesterButton = () => {
-		return (
-			<Button className="w-sm">
-				<Plus />
-				Add Future Semester
-			</Button>
-		);
-	};
+  const AddSemesterButton = () => {
+    return (
+      <Button className="w-sm ">
+        <Plus />
+        Add Future Semester
+      </Button>
+    );
+  };
 
-	return (
-		<VStack fill className="w-full">
-			<Title text="Degree Planner" />
-			<Wrap maxCols={2}>
-				{semesters.map((semester, index) => (
-					<SemesterCard
-						key={index}
-						semester={semester[0]}
-						courses={semester[1]}
-					/>
-				))}
-				<AddSemesterButton />
-			</Wrap>
-		</VStack>
-	);
+  return (
+    <VStack fill className="w-full">
+      <Title text="Degree Planner" />
+      <Wrap maxCols={2}>
+        {semesters.map((semester, index) => (
+          <SemesterCard
+            key={index}
+            semester={semester[0]}
+            courses={semester[1]}
+          />
+        ))}
+        <AddSemesterButton />
+      </Wrap>
+    </VStack>
+  );
 };
 
 const DegreePlannerPage = () => {
-	return (
-		<HStack fill x="between" className="h-full">
-			<MainContent />
-			<SidePanel />
-		</HStack>
-	);
+  return (
+    <HStack fill x="between" className="h-full">
+      <MainContent />
+      <SidePanel />
+    </HStack>
+  );
 };
 
 export default DegreePlannerPage;
--- entrypoints/degree-audit/components/semester-card.tsx
+++ entrypoints/degree-audit/components/semester-card.tsx
@@ -1,45 +1,45 @@
 import Dropdown, {
-	DropdownContent,
-	DropdownHeader,
+  DropdownContent,
+  DropdownHeader,
 } from "@/entrypoints/components/common/dropdown";
 import { HStack, VStack } from "@/entrypoints/components/common/helperdivs";
 import CourseCard from "@/entrypoints/components/course-card";
 import { Course } from "@/lib/general-types";
 
 export interface SemesterCardProps {
-	semester: string;
-	courses: Course[];
+  semester: string;
+  courses: Course[];
 }
 
 const SemesterCard = ({ semester, courses }: SemesterCardProps) => {
-	const totalHours = courses.reduce((acc, course) => acc + course.hours, 0);
+  const totalHours = courses.reduce((acc, course) => acc + course.hours, 0);
 
-	return (
-		<Dropdown
-			className="w-sm p-6 rounded-lg border border-gray-200 bg-[#FAFAF9]"
-			gap={6}
-		>
-			<DropdownHeader>
-				<HStack y="middle" x="between" fill>
-					<h2 className="text-lg text-dap-orange font-bold">{semester}</h2>
-					<h2 className="text-sm">{`${totalHours} hours`}</h2>
-				</HStack>
-			</DropdownHeader>
-			<DropdownContent className="w-full pb-6">
-				<VStack fill className="w-full" gap={4}>
-					{courses.map((course) => (
-						<CourseCard
-							className="w-full"
-							key={course.code}
-							fullName={course.name}
-							courseName={course.code}
-							color="orange"
-						/>
-					))}
-				</VStack>
-			</DropdownContent>
-		</Dropdown>
-	);
+  return (
+    <Dropdown
+      className="w-sm p-6 rounded-lg border border-gray-200 bg-[#FAFAF9]"
+      gap={6}
+    >
+      <DropdownHeader>
+        <HStack y="middle" x="between" fill>
+          <h2 className="text-lg text-dap-orange font-bold">{semester}</h2>
+          <h2 className="text-sm">{`${totalHours} hours`}</h2>
+        </HStack>
+      </DropdownHeader>
+      <DropdownContent className="w-full pb-6 max-h-86 overflow-y-auto">
+        <VStack fill className="w-full" gap={4}>
+          {courses.map((course) => (
+            <CourseCard
+              className="w-full"
+              key={course.code}
+              fullName={course.name}
+              courseName={course.code}
+              color="orange"
+            />
+          ))}
+        </VStack>
+      </DropdownContent>
+    </Dropdown>
+  );
 };
 
 export default SemesterCard;
