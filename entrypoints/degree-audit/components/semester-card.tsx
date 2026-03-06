a8ee898eda7c415ef02da090692d6fd64d8086f9
Miles F <miles.fritzmather@gmail.com>
2026-03-06 12:32:59 -0600

    final cleanup on some testing stuff

--- entrypoints/degree-audit/components/semester-card.tsx
+++ entrypoints/degree-audit/components/semester-card.tsx
@@ -9,7 +9,6 @@ import { Course, StringSemester } from "@/lib/general-types";
 import { cn } from "@/lib/utils";
 import { useDroppable } from "@dnd-kit/core";
 import { forwardRef } from "react";
-import DraggableCourseCard from "../planner-view/drag-and-drop/draggable-course-card";
 import { sortSemesters } from "../planner-view/semester-dropdowns";
 
 export interface SemesterCardProps {
@@ -33,34 +32,20 @@ const SemesterCardVisual = forwardRef<
     >
       <DropdownHeader>
         <HStack y="middle" x="between" fill>
-          <h2 className="text-lg text-dap-orange font-bold">
-            {semester} | {courses.length}
-          </h2>
+          <h2 className="text-lg text-dap-orange font-bold">{semester}</h2>
         </HStack>
       </DropdownHeader>
       <DropdownContent className="w-full max-h-86 overflow-y-auto">
-        {/* <SortableContext
-          items={courses.map((course) => course.id)}
-          strategy={verticalListSortingStrategy}
-        > */}
         <VStack fill className="w-full" gap={4}>
           {courses.length > 0 ? (
-            courses.map((course) =>
-              course.status !== "Completed" ? (
-                <DraggableCourseCard
-                  key={course.code}
-                  semester={semester}
-                  id={course.id}
-                  className="w-full"
-                />
-              ) : (
-                <CourseCard
-                  key={course.code}
-                  courseId={course.id}
-                  className="w-full"
-                />
-              ),
-            )
+            courses.map((course) => (
+              <CourseCard
+                key={course.code}
+                draggable={course.status !== "Completed"}
+                courseId={course.id}
+                className="w-full"
+              />
+            ))
           ) : (
             <VStack
               centered
@@ -71,7 +56,6 @@ const SemesterCardVisual = forwardRef<
             </VStack>
           )}
         </VStack>
-        {/* </SortableContext> */}
       </DropdownContent>
     </Dropdown>
   );
@@ -82,13 +66,7 @@ const SemesterCard = ({ semester, courses }: SemesterCardProps) => {
   // If the semester is in the past, make it non-droppable
   console.log("[semester]", semester, getCurrentSemester());
   if (sortSemesters(semester, getCurrentSemester()) < 0) {
-    return (
-      <SemesterCardVisual
-        semester={semester}
-        courses={courses}
-        className="bg-black/20"
-      />
-    );
+    return <SemesterCardVisual semester={semester} courses={courses} />;
   }
   return <DroppableSemesterCard semester={semester} courses={courses} />;
 };
